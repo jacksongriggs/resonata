5b1388e911be3238dc854b431d9f967721543ccd
jacksongriggs <114271129+jacksongriggs@users.noreply.github.com>
2023-08-29 02:49:31 +0800

    Fix: Macros now work as intended

--- src/notes/macros.rs
+++ src/notes/macros.rs
@@ -1,24 +1,24 @@
 /// A macro to create a note.
 #[macro_export]
 macro_rules! note {
-    // For note!(C);
-    ($name:ident) => { 
-        Note::build($name, Natural, 4)
-    };
     // For note!("C4");
-    ($name:expr) => {
+    ($name:literal) => {
         Note::from_str($name)
     };
+    // For note!(C);
+    ($name:expr) => {
+        Note::from_note_name_and_octave($name, 4)
+    };
     // For note!(C, 4);
-    ($name:ident $oct:literal) => {
-        Note::build($name, Natural, $oct)
+    ($name:expr, $oct:literal) => {
+        Note::from_note_name_and_octave($name, $oct)
     };
     // For note!(C, Flat(1));
-    ($name:ident $accidental:ident($num:literal)) => {
-        Note::build($name, $accidental($num), 4)
+    ($name:expr, $accidental:expr) => {
+        Note::build($name, $accidental, 4)
     };
     // For note!(C, Flat(1), 4);
-    ($name:ident $accidental:ident($num:literal) $oct:literal) => {
-        Note::build($name, $accidental($num), $oct)
+    ($name:expr, $accidental:expr, $oct:literal) => {
+        Note::build($name, $accidental, $oct)
     };
 }
