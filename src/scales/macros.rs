bc0c468fd4f26cdd9ec034d5f053547b53430bd0
jacksongriggs <114271129+jacksongriggs@users.noreply.github.com>
2023-08-31 00:12:55 +0800

    Scale: Fix duplicated macro

--- src/scales/macros.rs
+++ src/scales/macros.rs
@@ -1,11 +1,10 @@
 /// A macro to create a scale from a root note and a scale type.
 #[macro_export]
 macro_rules! scale {
-    ($scale_type:expr, $rot:literal) => {
-        Scale::from_steps($scale_type.as_steps()).rotated($rot)
+    ($scale_type:expr) => {
+        Scale::from_steps($scale_type.as_steps())
     };
     ($scale_type:expr, $rot:literal) => {
         Scale::from_steps($scale_type.as_steps()).rotated($rot)
     };
-
 }
